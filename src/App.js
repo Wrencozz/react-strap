3f77b5f059e03ace0d4624a5641d4e7ea97818dc
Wrencozz <fakychips@gmail.com>
2023-02-21 09:16:01 +0100

    strap

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
 import logo from './logo.svg';
-import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Badge, Navbar, Container, Nav } from 'react-bootstrap';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+     
+    <Navbar collapseOnSelect expand="lg" bg='dark' variant='dark' fixed='top'>
+    <Container>
+      <Navbar.Brand href='#home'>Ukázka</Navbar.Brand>
+      <Navbar.Toggle aria-controls='responsive-navbar-nav'/>
+      <Navbar.Collapse id="responsive-navbar-nav">
+      <Nav className='me-auto'>
+      <Nav.Link href='#home'>11111</Nav.Link>
+      <Nav.Link href='#home'>22222</Nav.Link>
+      </Nav>
+      </Navbar.Collapse>
+    </Container>
+    </Navbar>
+
     </div>
+  
+
+
   );
 }
 
